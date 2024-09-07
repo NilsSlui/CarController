7254761c05a471b79ed07a8c6aeee04241e48293
Nils <sluiter@outlook.com>
2024-09-07 14:33:08 +0200

    Cleanup

--- PrefabCarController.cs
+++ PrefabCarController.cs
@@ -3,241 +3,199 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PrefabCarController: MonoBehaviour {
+public class PrefabCarController : MonoBehaviour
+{
+    public GameObject WheelFrontLeft, WheelFrontRight, WheelRearLeft, WheelRearRight;
+    public WheelCollider WheelFrontLeft_Collider, WheelFrontRight_Collider, WheelRearLeft_Collider, WheelRearRight_Collider;
+    public Rigidbody CarRb;
+    public Text Speedo, DebugTextUI;
+    public GameObject WheelColliderPrefab, SkidMarker, Exhaust, ExhaustExplosion;
+    public MeshRenderer BreakLight;
+    public Material BreaklightActive, BreaklightInactive;
+    public float MaxSteerAngle = 37, MotorForce = 550;
+    public Vector3 CenterOfMass;
 
-  public GameObject WheelFrontLeft, WheelFrontRight;
-  public GameObject WheelRearLeft, WheelRearRight;
+    private GameObject WheelFrontLeft_WC, WheelFrontRight_WC, WheelRearLeft_WC, WheelRearRight_WC;
+    private GameObject SkidLeft, SkidRight;
+    private TouchDisplay touchDisplay;
+    private float steeringAngle, horizontalInput, verticalInput, speed;
+    private bool isSkidding, upToSpeed;
+    private int score, frames;
+    private float deltaAcceleration;
 
-  public float maxSteerAngle = 37;
-  public float motorTorque = 650;
-  public float m_steeringAngle;
-  public float motorForce = 550;
-
-  public bool Skidding;
-  public Vector3 LocalVelocity;
-
-  public Rigidbody CarRb;
-  public Vector3 CenterOfMass;
-
-  public WheelCollider WheelFrontLeft_Collider, WheelFrontRight_Collider;
-  public WheelCollider WheelRearLeft_Collider, WheelRearRight_Collider;
-
-  public GameObject WheelColliders;
-  private GameObject WheelFrontLeft_WC, WheelFrontRight_WC, WheelRearLeft_WC, WheelRearRight_WC;
-
-  public float m_horizontalInput, m_verticalInput;
-
-  public GameObject WheelColliderPrefab;
-
-  public TouchDisplay TouchDisplay;
-
-  public float Speed;
-  public Text Speedo;
-
-  public int Score;
-  public Text DebugTextUI;
-
-  //Skidding Object
-  public GameObject SkidMarker;
-  private GameObject SkidLeft;
-  private GameObject SkidRight;
-
-  //car accesoires
-  public GameObject Exhaust;
-  public GameObject ExhaustExplosion;
-  public MeshRenderer BreakLight;
-
-  //materials
-  public Material Breaklight_active;
-  public Material Breaklight_inactive;
-
-  //number of frames since last reset
-  public int frames;
-  //acceleration stored up to X frames
-  public float delta_acceleration;
-
-  private bool UpToSpeed;
-  //binds game wheels to variables
-  void FindWheels() {
-    WheelFrontLeft = GameObject.Find("WheelFrontLeft");
-    WheelFrontRight = GameObject.Find("WheelFrontRight");
-    WheelRearLeft = GameObject.Find("WheelRearLeft");
-    WheelRearRight = GameObject.Find("WheelRearRight");
-  }
-  //updateWheelPoses
-  public void UpdateWheelPoses() {
-    UpdateWheelPose(WheelFrontLeft_Collider, WheelFrontLeft.transform);
-    UpdateWheelPose(WheelFrontRight_Collider, WheelFrontRight.transform);
-    UpdateWheelPose(WheelRearLeft_Collider, WheelRearLeft.transform);
-    UpdateWheelPose(WheelRearRight_Collider, WheelRearRight.transform);
-  }
-  //sub method for each individual wheel
-  private void UpdateWheelPose(WheelCollider _collider, Transform _transform) {
-    Vector3 _pos = _transform.position;
-    Quaternion _quat = _transform.rotation;
-    _collider.GetWorldPose(out _pos, out _quat);
-    _transform.position = _pos;
-    _transform.rotation = _quat;
-  }
-
-  public void GetInput() {
-    //KEYBOARD AND MOUSE
-    m_horizontalInput = Input.GetAxis("Horizontal");
-    m_verticalInput = Input.GetAxis("Vertical");
-
-    //TOUCHSCREEN
-    //m_horizontalInput = TouchDisplay.HorizontalInput;
-    //m_verticalInput = TouchDisplay.VerticalInput;
-  }
-
-  public void DebugText() {
-    string tempdebugtext;
-    tempdebugtext = "H:" + Mathf.Round(m_horizontalInput * 100).ToString() + " V:" + Mathf.Round(m_verticalInput * 100).ToString();
-    DebugTextUI.text = tempdebugtext;
-  }
-  public void Steer() {
-    m_steeringAngle = maxSteerAngle * m_horizontalInput;
-    WheelFrontLeft_Collider.steerAngle = m_steeringAngle;
-    WheelFrontRight_Collider.steerAngle = m_steeringAngle;
-  }
-  public void Accelerate() {
-    WheelRearLeft_Collider.motorTorque = m_verticalInput * motorForce;
-    WheelRearRight_Collider.motorTorque = m_verticalInput * motorForce;
-  }
-  public void UpdateSpeed() {
-    Speed = CarRb.velocity.magnitude * 5;
-    Speedo.text = Mathf.Round(Speed).ToString();
-  }
-
-  //Check velocity and apply skidmarks when needed
-  private void IsSkidding() {
-    float _skidfactor = .7 f; //lower number, easier skid
-    LocalVelocity = transform.InverseTransformDirection(CarRb.velocity);
-    if (LocalVelocity.x > _skidfactor || LocalVelocity.x < -_skidfactor) {
-      Skid();
-    } else if (Speed > 6 && m_verticalInput < -.5 f) {
-      Skid();
-    } else if (Speed > 10 && m_horizontalInput > .8 f || Speed > 6 && m_verticalInput < -.8 f) {
-      Skid();
-    } else if (m_verticalInput == 1 && Speed < 4) {
-      Skid();
-    } else {
-      EndSkid();
+    private void Start()
+    {
+        touchDisplay = GameObject.Find("Main Camera").GetComponent<TouchDisplay>();
+        CarRb.centerOfMass = CenterOfMass;
+        FindWheels();
+        CreateWheelColliders();
+        CreateSkidPositions();
     }
-  }
 
-  private void Skid() {
-    if (Skidding == false) {
-      Instantiate(SkidMarker, SkidLeft.transform);
-      Instantiate(SkidMarker, SkidRight.transform);
-      Debug.Log("Skidding!");
-      Skidding = true;
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        ApplyBrakes();
+        UpdateWheelPoses();
+        UpdateSpeed();
+        CheckSkidding();
+        ExhaustBang();
+        SetBreakLight();
+        frames++;
     }
-  }
-  private void EndSkid() {
-    SkidLeft.transform.DetachChildren();
-    SkidRight.transform.DetachChildren();
-    Skidding = false;
-  }
 
-  //simulate backfire when acceleration has sudden drop
-  private void ExhaustBang() {
-    if (frames > 40) {
-      if (delta_acceleration > 0.7 & TouchDisplay.VerticalInput < 0.1) {
-        Bang();
-      }
-      delta_acceleration = m_verticalInput;
-      frames = 0;
+    private void LateUpdate()
+    {
+        UpdateDebugText();
     }
-  }
-  private void Bang() {
-    //ExhaustExplosion deactivates after X frames
-    ExhaustExplosion.SetActive(true);
-  }
 
-  //changes breaklight texture when breaking
-  private void SetBreakLight() {
-    if (m_verticalInput < -.1 f) {
-      BreakLight.material = Breaklight_active;
-    } else {
-      BreakLight.material = Breaklight_inactive;
+    void FindWheels()
+    {
+        WheelFrontLeft = GameObject.Find("WheelFrontLeft");
+        WheelFrontRight = GameObject.Find("WheelFrontRight");
+        WheelRearLeft = GameObject.Find("WheelRearLeft");
+        WheelRearRight = GameObject.Find("WheelRearRight");
     }
-  }
 
-  //increases drag when player reverses
-  private void Breaks() {
-    if (m_verticalInput < -.1 f) {
-      float _breakamount = (m_verticalInput * -1);
-      CarRb.drag = _breakamount;
-      CarRb.angularDrag = _breakamount;
-    } else {
-      CarRb.drag = 0.015 f;
-      CarRb.angularDrag = 0.027 f;
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
-  }
 
-  // Use this for initialization
-  void Start() {
-    GameObject maincam = GameObject.Find("Main Camera");
-    TouchDisplay = maincam.GetComponent < TouchDisplay > ();
-    CarRb.centerOfMass = CenterOfMass;
-    FindWheels();
-    CreateWheelColliders();
-    CreateSkidPositions();
-    UpToSpeed = false;
-  }
+    void Steer()
+    {
+        steeringAngle = MaxSteerAngle * horizontalInput;
+        WheelFrontLeft_Collider.steerAngle = steeringAngle;
+        WheelFrontRight_Collider.steerAngle = steeringAngle;
+    }
 
-  // Update is called once per frame
-  void FixedUpdate() {
-    GetInput();
-    Steer();
-    Accelerate();
-    Breaks();
-    UpdateWheelPoses();
-    UpdateSpeed();
-    IsSkidding();
-    ExhaustBang();
-    SetBreakLight();
-    frames++;
-  }
-  private void LateUpdate() {
-    DebugText();
-  }
+    void Accelerate()
+    {
+        WheelRearLeft_Collider.motorTorque = verticalInput * MotorForce;
+        WheelRearRight_Collider.motorTorque = verticalInput * MotorForce;
+    }
 
-  //creates wheel collider at each wheel position (4 wheels only)
-  void CreateWheelColliders() {
-    WheelFrontLeft_WC = Instantiate(WheelColliderPrefab, WheelFrontLeft.transform.position, Quaternion.identity);
-    WheelFrontLeft_WC.name = "WheelFrontLeft_WC";
-    WheelFrontLeft_WC.transform.parent = WheelColliders.transform;
-    WheelFrontLeft_Collider = WheelFrontLeft_WC.GetComponent < WheelCollider > ();
+    void ApplyBrakes()
+    {
+        if (verticalInput < -0.1f)
+        {
+            float brakeAmount = -verticalInput;
+            CarRb.drag = brakeAmount;
+            CarRb.angularDrag = brakeAmount;
+        }
+        else
+        {
+            CarRb.drag = 0.015f;
+            CarRb.angularDrag = 0.027f;
+        }
+    }
 
-    WheelFrontRight_WC = Instantiate(WheelColliderPrefab, WheelFrontRight.transform.position, Quaternion.identity);
-    WheelFrontRight_WC.name = "WheelFrontRight_WC";
-    WheelFrontRight_WC.transform.parent = WheelColliders.transform;
-    WheelFrontRight_Collider = WheelFrontRight_WC.GetComponent < WheelCollider > ();
+    void UpdateWheelPoses()
+    {
+        UpdateWheelPose(WheelFrontLeft_Collider, WheelFrontLeft.transform);
+        UpdateWheelPose(WheelFrontRight_Collider, WheelFrontRight.transform);
+        UpdateWheelPose(WheelRearLeft_Collider, WheelRearLeft.transform);
+        UpdateWheelPose(WheelRearRight_Collider, WheelRearRight.transform);
+    }
 
-    WheelRearLeft_WC = Instantiate(WheelColliderPrefab, WheelRearLeft.transform.position, Quaternion.identity);
-    WheelRearLeft_WC.name = "WheelRearLeft_WC";
-    WheelRearLeft_WC.transform.parent = WheelColliders.transform;
-    WheelRearLeft_Collider = WheelRearLeft_WC.GetComponent < WheelCollider > ();
+    void UpdateWheelPose(WheelCollider collider, Transform wheelTransform)
+    {
+        Vector3 position = wheelTransform.position;
+        Quaternion rotation = wheelTransform.rotation;
+        collider.GetWorldPose(out position, out rotation);
+        wheelTransform.position = position;
+        wheelTransform.rotation = rotation;
+    }
 
-    WheelRearRight_WC = Instantiate(WheelColliderPrefab, WheelRearRight.transform.position, Quaternion.identity);
-    WheelRearRight_WC.name = "WheelRearRight_WC";
-    WheelRearRight_WC.transform.parent = WheelColliders.transform;
-    WheelRearRight_Collider = WheelRearRight_WC.GetComponent < WheelCollider > ();
-  }
-  //set skidmark location based on wheel pos
-  private void CreateSkidPositions() {
-    float _SkidVertOffset = .25 f;
+    void UpdateSpeed()
+    {
+        speed = CarRb.velocity.magnitude * 5;
+        Speedo.text = Mathf.Round(speed).ToString();
+    }
 
-    SkidLeft = new GameObject();
-    SkidLeft.transform.position = new Vector3(WheelRearLeft.transform.position.x, WheelRearLeft.transform.position.y - _SkidVertOffset, WheelRearLeft.transform.position.z);
-    SkidLeft.name = "SkidL";
-    SkidLeft.transform.parent = this.transform;
+    void CheckSkidding()
+    {
+        float skidFactor = 0.7f;
+        Vector3 localVelocity = transform.InverseTransformDirection(CarRb.velocity);
+        if (Mathf.Abs(localVelocity.x) > skidFactor || (speed > 6 && verticalInput < -0.5f) || (speed > 10 && horizontalInput > 0.8f))
+        {
+            StartSkid();
+        }
+        else
+        {
+            EndSkid();
+        }
+    }
 
-    SkidRight = new GameObject();
-    SkidRight.transform.position = new Vector3(WheelRearRight.transform.position.x, WheelRearRight.transform.position.y - _SkidVertOffset, WheelRearRight.transform.position.z);
-    SkidRight.name = "SkidR";
-    SkidRight.transform.parent = this.transform;
-  }
+    void StartSkid()
+    {
+        if (!isSkidding)
+        {
+            Instantiate(SkidMarker, SkidLeft.transform);
+            Instantiate(SkidMarker, SkidRight.transform);
+            isSkidding = true;
+        }
+    }
+
+    void EndSkid()
+    {
+        SkidLeft.transform.DetachChildren();
+        SkidRight.transform.DetachChildren();
+        isSkidding = false;
+    }
+
+    void ExhaustBang()
+    {
+        if (frames > 40 && deltaAcceleration > 0.7f && touchDisplay.VerticalInput < 0.1f)
+        {
+            ExhaustExplosion.SetActive(true);
+        }
+        deltaAcceleration = verticalInput;
+        frames = 0;
+    }
+
+    void SetBreakLight()
+    {
+        BreakLight.material = verticalInput < -0.1f ? BreaklightActive : BreaklightInactive;
+    }
+
+    void UpdateDebugText()
+    {
+        DebugTextUI.text = "H: " + Mathf.Round(horizontalInput * 100).ToString() + " V: " + Mathf.Round(verticalInput * 100).ToString();
+    }
+
+    void CreateWheelColliders()
+    {
+        WheelFrontLeft_WC = Instantiate(WheelColliderPrefab, WheelFrontLeft.transform.position, Quaternion.identity, WheelColliders.transform);
+        WheelFrontLeft_WC.name = "WheelFrontLeft_WC";
+        WheelFrontLeft_Collider = WheelFrontLeft_WC.GetComponent<WheelCollider>();
+
+        WheelFrontRight_WC = Instantiate(WheelColliderPrefab, WheelFrontRight.transform.position, Quaternion.identity, WheelColliders.transform);
+        WheelFrontRight_WC.name = "WheelFrontRight_WC";
+        WheelFrontRight_Collider = WheelFrontRight_WC.GetComponent<WheelCollider>();
+
+        WheelRearLeft_WC = Instantiate(WheelColliderPrefab, WheelRearLeft.transform.position, Quaternion.identity, WheelColliders.transform);
+        WheelRearLeft_WC.name = "WheelRearLeft_WC";
+        WheelRearLeft_Collider = WheelRearLeft_WC.GetComponent<WheelCollider>();
+
+        WheelRearRight_WC = Instantiate(WheelColliderPrefab, WheelRearRight.transform.position, Quaternion.identity, WheelColliders.transform);
+        WheelRearRight_WC.name = "WheelRearRight_WC";
+        WheelRearRight_Collider = WheelRearRight_WC.GetComponent<WheelCollider>();
+    }
+
+    void CreateSkidPositions()
+    {
+        float skidVertOffset = 0.25f;
+
+        SkidLeft = new GameObject("SkidL", typeof(Transform));
+        SkidLeft.transform.position = new Vector3(WheelRearLeft.transform.position.x, WheelRearLeft.transform.position.y - skidVertOffset, WheelRearLeft.transform.position.z);
+        SkidLeft.transform.parent = this.transform;
+
+        SkidRight = new GameObject("SkidR", typeof(Transform));
+        SkidRight.transform.position = new Vector3(WheelRearRight.transform.position.x, WheelRearRight.transform.position.y - skidVertOffset, WheelRearRight.transform.position.z);
+        SkidRight.transform.parent = this.transform;
+    }
 }
